be50fea91a4b2353575e59285feca472ce1f1d28
Pranjal Sengar <pranjal.s.2210@gmail.com>
2023-12-12 18:36:02 +0530

    Built

--- routes/db.js
+++ routes/db.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Pranjal@123",
+  password: "Pranjal@1234",
   database: "sams_project",
   waitForConnections: true,
   connectionLimit: 100,
